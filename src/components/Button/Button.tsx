284b71fead425807f63d66b03e7a6043b8c873b2
aurimas1997 <aurimas.tunaitis@interactio.io>
2021-12-06 15:44:24 +0200

    Initial commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+interface ButtonProps {
+    label: string;
+}
+const Button: React.FC<ButtonProps> = props => {
+    return (
+        <button>{props.label}</button>
+    );
+};
+
+export default Button;

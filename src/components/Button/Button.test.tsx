da37b5a4f58592cb9782f1492786850984255033
aurimas1997 <aurimas.tunaitis@interactio.io>
2021-12-07 11:18:36 +0200

    Update Button test cases

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -4,7 +4,11 @@ import { render } from '@testing-library/react';
 import Button from "./Button";
 
 describe('Button', () => {
-    test('Renders the Button component', () => {
-        render(<Button label="Hello world" />)
+    test('Renders Primary Button component', () => {
+        render(<Button variant="primary" label="Primary" />)
+    });
+
+    test('Renders Secondary Button component', () => {
+        render(<Button variant="secondary" label="Secondary" />)
     });
 });

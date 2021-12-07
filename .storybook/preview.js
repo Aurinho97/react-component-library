a3f7ee194bd6d9ec7609e147f4a5fd273dbd1ae0
aurimas1997 <aurimas.tunaitis@interactio.io>
2021-12-07 13:39:38 +0200

    Updates

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,4 @@
-import '../src/styles/index.module.scss';
+import '../dist/esm/index.css'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,1 @@
-export { default as globalStyles } from '../styles/index.module.scss';
 export { default as Button } from './Button';
